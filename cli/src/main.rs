c3990d5fbb123cdd40c9651979a3d486d934027a
Fernando Cejas <android10@fernandocejas.com>
2023-06-08 23:57:55 +0200

    feat: add help to the different command options

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -3,8 +3,17 @@ use clap::Parser;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)] // Read from `Cargo.toml`
 struct Cli {
-    #[arg(short, long)]
-    install: Option<String>,
+    #[arg(short = 'b', long = "bootstrap", help = "Initialize 'dotfiles' directory")]
+    bootstrap: bool,
+
+    #[arg(short = 'l', long = "list-profiles", help = "List existing 'doty' profiles")]
+    list_profiles: bool,
+
+    #[arg(name= "PROFILE", short = 'i', long = "install-profile", help = "Install from 'doty.<PROFILE>.toml' file")]
+    install_profile: Option<String>,
+
+    #[arg(short = 's', long = "sanity-check", help = "Performs 'dotfiles' dir sanity check")]
+    sanity_check: bool,
 }
 
 fn main() {
