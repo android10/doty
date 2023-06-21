cbb23492e8c052cc36f0101090f4c8dcd47c0727
Fernando Cejas <android10@fernandocejas.com>
2023-06-21 17:53:15 +0200

    feat: add FeatureSanity

--- cli/src/features/feature_sanity.rs
+++ cli/src/features/feature_sanity.rs
@@ -1,1 +1,13 @@
+use crate::core::feature::Feature;
 
+#[derive(Debug)]
+pub struct FeatureSanity { }
+
+impl Feature for FeatureSanity {
+    fn run(&self) {
+        match self.dotfiles_dir() {
+            Ok(_) => println!("doty: sanity!!!"),
+            Err(_) => println!("ERROR!")
+        }
+    }
+}
